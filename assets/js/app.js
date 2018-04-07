866157908ff89efeea60353a6378fe5a560eb2d3
Cole Hansen <cole.hansen@gmail.com>
2018-04-07 10:23:29 -0700

    Add HTML structure and firebase link

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+$(document).on("ready", function () {
+
+    var config = {
+        apiKey: "AIzaSyCoMz1y-KDjBZOy91LlGUcELkkTaKsfz8Y",
+        authDomain: "train-schedule-7bac0.firebaseapp.com",
+        databaseURL: "https://train-schedule-7bac0.firebaseio.com",
+        projectId: "train-schedule-7bac0",
+        storageBucket: "train-schedule-7bac0.appspot.com",
+        messagingSenderId: "555804970957"
+    };
+    firebase.initializeApp(config);
+
+    
+
+});
